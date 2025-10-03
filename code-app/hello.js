56f2871bd04217fc62bd66c3f131b223492e18ca
code-tofu <benjamin.ftc@gmail.com>
2025-10-04 01:43:02 +0800

    Test Code-App

---
+++ code-app/hello.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => {
+    console.log(response.data);
+    console.log("success");
+  })
+  .catch(error => {
+    console.error(error);
+  });
